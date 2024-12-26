a9fd0cadbb5f3e96a5a07b363195303bbd2920d1
Nishit Vataliya <nishitvataliya003@gmail.com>
2024-12-26 16:49:14 +0530

    add video Caraosel components

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,6 +27,7 @@ const Hero = () => {
   window.addEventListener("resize", handleVideoSrcSet);
   useGSAP(() => {
     gsap.to("#hero", { opacity: 1, delay: 1.5 });
+    gsap.to("#cta", { opacity: 1, y: -50, delay: 1.5 });
   }, []);
   return (
     <section className="w-full nav-height bg-black relative">
@@ -45,6 +46,15 @@ const Hero = () => {
             <source src={videoSrc} type="video/mp4" />
           </video>
         </div>
+      </div>
+
+      <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+        <a href="#highlights" className="btn">
+          Buy
+        </a>
+        <p className="font-normal text-xl">
+          From $199/month or $999
+        </p>
       </div>
     </section>
   );
